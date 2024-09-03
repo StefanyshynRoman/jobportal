b2bac25b436ac04f48f60b62bbcb3691ea322b72
roman <romanpz051@gmail.com>
2024-09-03 10:23:54 +0200

    0.0.0.6 -bug with email

--- src/main/java/com/stefanyshyn/jobportal/controller/UsersController.java
+++ src/main/java/com/stefanyshyn/jobportal/controller/UsersController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UsersController {
@@ -23,17 +24,29 @@ public class UsersController {
         this.usersTypeService = usersTypeService;
         this.usersService = usersService;
     }
+
     @GetMapping("/register")
-    public String register(Model model){
+    public String register(Model model) {
+        return getRegister(model);
+    }
+
+    @PostMapping("/register/new")
+    public String userRegistration(@Valid Users users, Model model) {
+        Optional<Users> optionalUsers = usersService.getUserByEmail(users.getEmail());
+        if (optionalUsers.isPresent()){
+            model.addAttribute("error",
+                    "Email already registered, try to login or registration with other email.");
+            return getRegister(model);
+        }
+        usersService.addNew(users);
+        return "dashboard";
+
+    }
+
+    private String getRegister(Model model) {
         List<UsersType> usersTypes = usersTypeService.getAll();
         model.addAttribute("getAllTypes", usersTypes);
         model.addAttribute("user", new Users());
         return "register";
-    }
-    @PostMapping("/register/new")
-    public String userRegistration(@Valid Users users){
-        usersService.addNew(users);
-        return "dashboard";
-
     }
 }
